5c113471b1701d5816d833431a491883f26b4430
Zero <zeroonegames89@gmail.com>
2023-02-24 23:14:14 -0500

    added 4v4 and 6s + fixed the name issue

--- OFWelcomeBot.py
+++ OFWelcomeBot.py
@@ -5,12 +5,20 @@ intents = discord.Intents.all()
 intents.members = True
 bot = commands.Bot(command_prefix='!',intents=intents)
 
-serverID = 0 #Server ID goes here
+serverID = ""
+with open('serverID.txt') as f:
+    serverID = f.readline()
+serverID = int(serverID)
+
 bot.oldFartServer = ""
 bot.twosMakerChannel = ""
 bot.threesMakerChannel = ""
+bot.fourssMakerChannel = ""
+bot.sixesMakerChannel = ""
 bot.twosCategory = ""
 bot.threesCategory = ""
+bot.foursCategory = ""
+bot.sixesCategory = ""
 
 @bot.event
 async def on_ready():
@@ -24,6 +32,9 @@ async def on_ready():
             print("Found server")
             bot.oldFartServer = g
             oldFartServer = bot.oldFartServer
+        else :
+            print("server not found")
+
     #create categories
     if not guild_has_category(oldFartServer, "2v2"):
         print("making 2v2 category...")
@@ -33,6 +44,7 @@ async def on_ready():
          #create temp vc makers
         await oldFartServer.create_voice_channel("Create new 2v2",category=bot.twosCategory)
 
+    #create 3v3 category
     if not guild_has_category(oldFartServer, "3v3"):
         print("making 3v3 category...")
         await oldFartServer.create_category("3v3")
@@ -41,15 +53,40 @@ async def on_ready():
          #create temp vc makers
         await oldFartServer.create_voice_channel("Create new 3v3",category=bot.threesCategory)
 
+    #create 4v4 category
+    if not guild_has_category(oldFartServer, "4v4"):
+        print("making 4v4 category...")
+        await oldFartServer.create_category("4v4")
+        
+        bot.foursCategory = get_category(oldFartServer, "4v4")
+         #create temp vc makers
+        await oldFartServer.create_voice_channel("Create new 4v4",category=bot.foursCategory)
+
+    #create 6mans category
+    if not guild_has_category(oldFartServer, "6mans"):
+        print("making 6mans category...")
+        await oldFartServer.create_category("6mans")
+        
+        bot.sixesCategory = get_category(oldFartServer, "6mans")
+         #create temp vc makers
+        await oldFartServer.create_voice_channel("Create new 6mans",category=bot.sixesCategory)
+
+
     #get categories
     bot.twosCategory = get_category(oldFartServer, "2v2")
     bot.threesCategory = get_category(oldFartServer, "3v3")
+    bot.foursCategory = get_category(oldFartServer, "4v4")
+    bot.sixesCategory = get_category(oldFartServer, "6mans")
     #get creator channels
     for c in oldFartServer.channels:
         if c.name == "Create new 2v2":
             bot.twosMakerChannel = c
         if c.name == "Create new 3v3":
             bot.threesMakerChannel = c
+        if c.name == "Create new 4v4":
+            bot.foursMakerChannel = c
+        if c.name == "Create new 6mans":
+            bot.sixesMakerChannel = c
 
 
    
@@ -72,49 +109,79 @@ async def on_voice_state_update(member, before, after):
         guild = member.guild
         vcGen2v2 = bot.twosMakerChannel
         threesMakerChan = bot.threesMakerChannel
+        foursMakerChan = bot.foursMakerChannel
+        sixesMakerChan = bot.sixesMakerChannel
+
+        #TODO add 6mans
+        if before.channel != None and "Max #" in before.channel.name or before.channel != None and "6mans #" in before.channel.name:
+            if len(before.channel.members) == 0:
+                await before.channel.delete()
+
+        
         
         if after.channel == vcGen2v2:
             #create 2 max VC
-            #pull user into new VC
             cnum = 1
             cpos = vcGen2v2.position + 1
             for c in guild.channels:
                 if c != None and "2 Max" in c.name:
+                    await c.edit(name = "2 Max #" + str(cnum))
                     cnum = cnum+1
                     cpos = cpos +1
             await guild.create_voice_channel("2 Max #" + str(cnum), position=cpos, category=bot.twosCategory, user_limit=2)
+            #pull user into new VC
             for c in guild.channels:
                 if c != None and c.name == "2 Max #" + str(cnum):
                     await member.move_to(c)
-        if before.channel != None and "2 Max #" in before.channel.name:
-            if len(before.channel.members) == 0:
-                await before.channel.delete()
 
         if after.channel == threesMakerChan:
             #create 3 max VC
-            #pull user into new VC
             cnum = 1
             cpos = threesMakerChan.position + 1
             for c in guild.channels:
                 if c != None and "3 Max" in c.name:
+                    await c.edit(name = "3 Max #" + str(cnum))
                     cnum = cnum+1
                     cpos = cpos +1
             await guild.create_voice_channel("3 Max #" + str(cnum), position=cpos, category=bot.threesCategory, user_limit=3)
+            #pull user into new VC
             for c in guild.channels:
                 if c != None and c.name == "3 Max #" + str(cnum):
                     await member.move_to(c)
-        if before.channel != None and "3 Max #" in before.channel.name:
-            if len(before.channel.members) == 0:
-                await before.channel.delete()
+        
+        if after.channel == foursMakerChan:
+            #create 4 max VC
+            cnum = 1
+            cpos = foursMakerChan.position + 1
+            for c in guild.channels:
+                if c != None and "4 Max" in c.name:
+                    await c.edit(name = "4 Max #" + str(cnum))
+                    cnum = cnum+1
+                    cpos = cpos +1
+            await guild.create_voice_channel("4 Max #" + str(cnum), position=cpos, category=bot.foursCategory, user_limit=4)
+            #pull user into new VC
+            for c in guild.channels:
+                if c != None and c.name == "4 Max #" + str(cnum):
+                    await member.move_to(c)
+
+        if after.channel == sixesMakerChan:
+            #create 6 max VC
+            cnum = 1
+            cpos = sixesMakerChan.position + 1
+            for c in guild.channels:
+                if c != None and "6mans #" in c.name:
+                    await c.edit(name = "6mans #" + str(cnum))
+                    cnum = cnum+1
+                    cpos = cpos +1
+            await guild.create_voice_channel("6mans #" + str(cnum), position=cpos, category=bot.sixesCategory, user_limit=6)
+            #pull user into new VC
+            for c in guild.channels:
+                if c != None and c.name == "6mans #" + str(cnum):
+                    await member.move_to(c)
                 
 @bot.event
 async def on_guild_channel_create(channel):
     print(channel.name + " was created!")
-    if channel.name == "2v2":
-        bot.twosMakerChannel = channel
-    if channel.name == "Create new 3v3":
-        bot.threesMakerChannel = channel
-
     
 #@bot.event
 #async def on_member_join(member):
--- README.md
+++ README.md
@@ -4,3 +4,20 @@ Bot for Old Farts
 # Features
 Welcome message
 temp voice channels
+
+# Installation 
+You need to have python installed and also install pycord
+
+you can do this using pip with the command: 
+python3 -m pip install -U py-cord
+
+# setup
+The bot requires 2 files containing the ID for your server and the secret token for your bot
+
+Your server ID can be obtained by right clicking your server icon in discord and selecting "copy ID"
+This should be saved in a txt file named "serverID.txt"
+
+Your secret bot token is obtained from your discord developer portal when you create a new bot, this token tells the code which discord bot it will be controlling.
+Make sure to keep this token a secret, this is why I have written the code to store that information seperately so you wont even need to share it with me. 
+
+Once these 2 files are ready and in the same folder as the python bot just run the bot and it should handle the rest. 
