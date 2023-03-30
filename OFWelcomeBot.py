0537db79a24a93cbe4e6143eb095c009ba18433c
Zero <zeroonegames89@gmail.com>
2023-03-30 09:53:43 -0400

    added channel rename and reordering

--- OFWelcomeBot.py
+++ OFWelcomeBot.py
@@ -93,10 +93,35 @@ async def on_ready():
             bot.foursMakerChannel = c
         if c.name == "➕ Create 6's VC":
             bot.sixesMakerChannel = c
+    
+
+async def update_channels():
+    try:
+        await rename_and_reorder("2 Max", bot.twosMakerChannel.position +1)
+    except:
+        print("failed to reorder 2s")
+    try:
+        await rename_and_reorder("3 Max", bot.threesMakerChannel.position +1)
+    except:
+        print("failed to reorder 3s")
+    try:
+        await rename_and_reorder("4 Max", bot.foursMakerChannel.position +1)
+    except:
+        print("failed to reorder 4s")
+    try:
+        await rename_and_reorder("6 Max", bot.sixesMakerChannel.position +1)
+    except:
+        print("failed to reorder 6s")
+ 
+@bot.event
+async def on_message(msg):
+    await update_channels()
+    await bot.process_commands(msg)
+    
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-       
+
     #delete any empty VCs that we are making
     if before.channel != None and "Max #" in before.channel.name:
         if len(before.channel.members) == 0:
@@ -122,46 +147,6 @@ async def on_voice_state_update(member, before, after):
         cpos = bot.sixesMakerChannel.position + 1
         await create_custom_channel("6 Max", cpos, bot.sixesCategory,6,member)
 
-@bot.event
-async def on_message(msg):
-    await bot.process_commands(msg)
-    return
-    if msg.author.id == bot.user.id:
-        if "LFG" in msg.content:
-            qMax_index = msg.content.find("Game Mode: ")
-            qMax = msg.content[qMax_index+11:qMax_index+12]
-            QeueSystem.create_new_queue(msg.id,qMax,bot.activeQueues)
-
-@bot.event
-async def on_raw_reaction_add(payload):
-    return
-    if QeueSystem.is_queue_message(payload.message_id,bot.activeQueues):
-        #TODO handle queue
-
-        #get this message
-        mChan = await bot.oldFartServer.fetch_channel(payload.channel_id)
-        qMessage = await mChan.fetch_message(payload.message_id)
-        
-        #get the queue object
-        for q in bot.activeQueues:
-            if q.id == payload.message_id:
-                queueData = q
-
-        #get the reactions to the message
-        users = set()
-        for reaction in qMessage.reactions:
-            async for user in reaction.users():
-                users.add(user)
-        #print(f"users: {', '.join(user.name for user in users)}")
-        #print(users)
-         
-        #compare the number to the max for this queue
-        #if the number of reactors is high enough DM each user
-        if len(users) == int(queueData.maxPlayers):
-            print("max players achieved!")
-            for user in users:
-                await user.send("Queue Pop! Head on over to RLOF and join your party in VC if you have not already!")
-                #TODO create a reserverd VC that only these players can join and send them a direct invite link to that VC instead of this message
 
 async def create_custom_channel(cName, position, category, userLimit, player):
     cnum = 1
@@ -193,68 +178,17 @@ def get_category(guild, cat):
     return
 
 
-
-######## Start of custom commands##################################################
-
-#LFG MAKER PRE ALPHA
-@bot.command()
-async def LFG(ctx, maxPlayers):
-    return
-    if not maxPlayers.isdigit():
-        await ctx.send("Please enter a valid number between 1 and 6")
-        return
-    if int(maxPlayers) > 6 or int(maxPlayers) < 1:
-        await ctx.send("Please enter a valid number between 1 and 6")
-        return
-    guild = ctx.guild
-    player = ctx.author
-    rank = QeueSystem.get_player_rank(player)
-    reg = QeueSystem.get_player_region(player)
-
-    await ctx.send("```LFG: \nRank: " + rank + "\nRegion: " + reg + "\nGame Mode: " + maxPlayers + "s\nReact to join Queue...```"  )
-
-
-######## END OF COMMANDS ##########################################################
+async def rename_and_reorder(cName, cPos):
+    cnum = 1
+    #for channel in server
+    for c in bot.oldFartServer.channels:
+        if c != None and cName in c.name:
+            await c.edit(name = cName +" #" + str(cnum), position = cPos)
+            cnum = cnum +1
+            cPos = cPos +1
 
 
 with open('token.txt') as f:
     TOKEN = f.readline()
 
 bot.run(TOKEN)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#@bot.event
-#async def on_member_join(member):
-#    # channel to print to
-#    printChan = bot.get_channel(12345679)
-#    await printChan.send( "Welcome " + member.mention + " Please head over to " + printChan.mention + " to set up your profile")
-
-
-
-# user does some command like "!LFG Diamond2 2v2 VC" (need to think of a way to make this more user friendly)
-#bot creates voice channel max 2/3 based on mode selected.
-#bot pulls user into VC
-#bot posts link to channel in LFG chat + info about the LFG
-
-
-
-
-#### REMEMBER if you add on message you need to include "await bot.process_commands(message)"
